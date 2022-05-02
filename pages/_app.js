9d4b4abc3c48a2d56e342aca112da2ba809ebbc9
Mark Murphy <mark.murphy@bigcommerce.com>
2022-05-02 13:02:41 -0500

    working template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/styles.css';
+import '../styles/styles-custom.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
